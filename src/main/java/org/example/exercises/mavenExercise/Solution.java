7d37e906cd12eee82a526d5aba4dc094026277e3
Bohdan Krasin <bohdan.krasin@gds.ey.com>
2023-04-21 12:15:42 +0200

    refactor due to PR

--- src/main/java/org/example/exercises/mavenExercise/Solution.java
+++ src/main/java/org/example/exercises/mavenExercise/Solution.java
@@ -1,11 +1,13 @@
 package org.example.exercises.mavenExercise;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Solution {
-    private static final Logger log = LogManager.getLogger(Solution.class);
+    private static final Logger log = LoggerFactory.getLogger(Solution.class);
+
     public static void main(String[] args) {
 
         log.info("Starting application");
